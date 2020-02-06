e9831676199c1a332baee3dca0ae608ae2d2c4b4
Ivan C <ivedcastr@gmail.com>
2020-02-06 18:02:43 -0500

    added extra nfl plot, modified tufte background

--- R/2020-02-04-nfl-attendance.R
+++ R/2020-02-04-nfl-attendance.R
@@ -90,12 +90,71 @@ ggplot(standings, aes(as.factor(year),
              size = 1.3) +
   theme_minimal() +
   theme(axis.text = element_text(color = "black"),
-        panel.background = element_rect(color = "#fffff8"),
+        panel.background = element_rect(fill = "#fffff8",
+                                        color = "#fffff8"),
         panel.grid.major.y = element_blank()) +
   labs(x = NULL,
        y = "Season margin of victory") +
   ggtitle("The NY Giants have the lowest margins of victory \namong Superbowl winners in the past 20 seasons")
 
-ggsave("plots/2020-02-04-nfl-attendance/margins.jpeg",
-       plot = last_plot())
+#ggsave("plots/2020-02-04-nfl-attendance/margins.jpeg",
+#       plot = last_plot())
+
+
+sb_winners <- standings %>% 
+  filter(sb_winner == "Won Superbowl") %>% 
+  distinct(team_name) %>% 
+  pull()
+
+
+merged_att <- attendance %>% 
+  select(team, team_name,
+         year, home, away,
+         total) %>% 
+  distinct() %>% 
+  left_join(
+    standings %>% 
+      select(team, team_name, year, sb_winner),
+    by = c("team", "team_name", "year")
+  )
+
+
+merged_att %>% 
+  filter(team_name %in%
+           sb_winners) %>% 
+  separate(year, into = 
+             c("cent", "year"),
+           sep = 2) %>% 
+  mutate(year = paste0("'", year),
+         year = as.factor(year)) %>% 
+  ggplot(aes(year, away)) +
+  geom_point(aes(color = sb_winner)) +
+  geom_segment(aes(x = year, xend = year,
+                   y = 0, yend = away,
+                   color = sb_winner)) +
+  facet_wrap(~team_name,
+             ncol = 2) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 8,
+                                   face = "bold.italic"),
+        axis.text = element_text(color = "black"),
+        panel.grid = element_blank(),
+        legend.position = "top",
+        axis.title = element_text(hjust = 1,
+                                  face = "bold.italic"),
+        axis.line.x.bottom = element_line()) +
+  scale_y_continuous(labels = scales::comma) +
+  scale_color_manual(values = 
+                       c("No Superbowl" = "Gray80",
+                         "Won Superbowl" = "#001959")) +
+  labs(color = "",
+       y = "Away attendance",
+       x = "")
+
+
+#ggsave("plots/2020-02-04-nfl-attendance/away_attend.jpeg",
+#       plot = last_plot())
+
+
+
 
